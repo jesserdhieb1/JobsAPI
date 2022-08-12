f3787344fe5205287ab5c33ad59267964e9faa6c
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-13 00:35:00 +0200

    commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,16 @@
 const {StatusCodes}=require('http-status-codes')
 const User = require('../models/User')
 const {BadRequestError}=require('../errors')
-const crypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const register  = async (req,res)=>{
-    const{name,email,password}=req.body
+    // const{name,email,password}=req.body
     // if(!name || !email || !password){
     //     throw new BadRequestError('please provide all your credentials')
     // }
     const user = await User.create({...req.body})
-    res.status(StatusCodes.CREATED).json(user)
+    const token = user.createJWT();
+    res.status(StatusCodes.CREATED).json({user: {name:user.name},token})
 }
 
 const login  = async (req,res)=>{
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose =require ('mongoose')
 const crypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const UserSchema =  mongoose.Schema({
     name:{
@@ -26,4 +27,7 @@ UserSchema.pre('save',async function(){
     this.password=await crypt.hash(this.password,salt)
 })
 
+UserSchema.methods.createJWT = function (){
+   return jwt.sign({userId: this._id, username: this.name}, 'JWTSecret', {expiresIn: '30d'});
+}
 module.exports=mongoose.model('User',UserSchema)
