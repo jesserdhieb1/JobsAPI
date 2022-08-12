21a68af396da112a49c978e0a5401728337808ed
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-12 19:36:53 +0200

    commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,9 @@
 const {StatusCodes}=require('http-status-codes')
+const User = require('../models/User')
 
 const register  = async (req,res)=>{
-    res.status(StatusCodes.OK).send('user registered')
+    const user = await User.create({...req.body})
+    res.status(StatusCodes.CREATED).json(user)
 }
 
 const login  = async (req,res)=>{
