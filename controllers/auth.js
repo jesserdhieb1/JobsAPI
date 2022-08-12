99087e9c84db24a5233445dfef10b4657fff4046
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-12 22:12:17 +0200

    commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,11 @@
 const {StatusCodes}=require('http-status-codes')
 const User = require('../models/User')
-
+const {BadRequestError}=require('../errors')
 const register  = async (req,res)=>{
+    const{name,email,password}=req.body
+    if(!name || !email || !password){
+        throw new BadRequestError('please provide all your credentials ')
+    }
     const user = await User.create({...req.body})
     res.status(StatusCodes.CREATED).json(user)
 }
