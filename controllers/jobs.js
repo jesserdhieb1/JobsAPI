eeec0ad947bde5bfc41a264d1e0e472e67e236bf
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-13 06:24:36 +0200

    commit

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -9,7 +9,12 @@ const getAllJobs  = async (req,res)=>{
 }
 
 const getJob = async (req,res)=>{
-    res.status(StatusCodes.OK).send('get single jobs')
+    const {user: {userId},params:{id:jobId}}=req
+    const job = await Job.findOne({_id:jobId,createdBy:userId})
+    if (!job){
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({job})
 }
 
 const createJob  = async (req,res)=>{
@@ -19,7 +24,15 @@ const createJob  = async (req,res)=>{
 }
 
 const updateJob  = async (req,res)=>{
-    res.status(StatusCodes.OK).send('job updated')
+    const {body:{company,position},user: {userId},params:{id:jobId}}=req
+    if (company==='' || position===''){
+        throw new Error('Company or position are missing')
+    }
+    const job = await Job.findOneAndUpdate({_id:jobId,createdBy:userId},req.body,{runValidators:true,new:true})
+    if (!job){
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({job})
 }
 
 const deleteJob  = async (req,res)=>{
