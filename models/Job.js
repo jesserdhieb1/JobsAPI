25934fb3a1fb152889c5658a9c36c1019c40ab08
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-13 03:40:50 +0200

    commit

---
+++ models/Job.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const JobSchema =mongoose.Schema({
+    company:{
+        type:String,
+        required:[true,'please provide a company name'],
+        maxLength:50
+    },
+    position:{
+        type:String,
+        required:[true,'please provide a position'],
+        maxLength:100
+    },
+    status:{
+        type:String,
+        enum:['interview','declined','pending'],
+        default:'pending'
+    },
+    createdBy:{
+        type:mongoose.Types.ObjectId,
+        ref:'User',
+        required:[true,'please provide a user']
+    }
+},{timestamps:true}
+)
+
+module.exports=mongoose.model('Job',JobSchema)
