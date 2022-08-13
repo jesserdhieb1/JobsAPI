20aa025229b569faacf791e93b1f797e0432f43f
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-13 06:54:48 +0200

    commit

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,11 +1,19 @@
 const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err)
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  let CustomError = {
+    statusCode:err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg:err.message || 'Something went wrong try again later'
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message })
+  // }
+  if (err.code && err.code===11000){
+    CustomError.msg=`Duplicate value entered for email failed please choose another value`
+    CustomError.statusCode= 400
+  }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  return res.status(CustomError.statusCode).json({ msg: CustomError.msg })
 }
 
 module.exports = errorHandlerMiddleware
