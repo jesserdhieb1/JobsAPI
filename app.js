5778cb81bfa1e473badb5b5fa9b01ab08c49de74
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-15 01:47:33 +0200

    commit

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
 require('express-async-errors');
+const helmet = require('helmet')
+const cors =require('cors')
+const xss = require('xss-clean')
+const rateLimit  =('express-rate-limit')
+
 const express = require('express');
 const app = express();
 
@@ -13,9 +18,15 @@ const jobsRoutes = require('./routes/jobs')
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
-
+app.set('trust proxy', 1);
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+}))
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 app.use(express.json());
-// extra packages
 
 // routes
 app.use('/api/v1/jobs',authenticate,jobsRoutes)
