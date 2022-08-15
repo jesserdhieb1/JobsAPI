2304ea9e2c82b484d9f1c813cdbd00f0be9b30e0
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-15 02:58:10 +0200

    commit

--- app.js
+++ app.js
@@ -20,22 +20,18 @@ const jobsRoutes = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 app.set('trust proxy', 1);
-console.log(rateLimit)
-const limiter = rateLimit({
+app.use(rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
-app.use(limiter)
-// app.use(rateLimit({
-//   windowMs: 15 * 60 * 1000, // 15 minutes
-//   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-// }))
+}))
 app.use(helmet())
 app.use(cors())
 app.use(xss())
 app.use(express.json());
+
+app.get('/',(req,res)=>{
+  res.status(200).send('Jobs API')
+})
 
 // routes
 app.use('/api/v1/jobs',authenticate,jobsRoutes)
